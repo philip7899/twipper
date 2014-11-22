c2019a08c64629167b27cfdaf79371f55f319ae2
Philip Greenwald <philipgreenwald@philips-MacBook-Pro.local>
2014-11-22 16:43:38 -0500

    created newsfeed

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,10 @@
 class TweetsController < ApplicationController
+
+	before_action :authenticate_user!
+
 	def new
 		@tweet = Tweet.new
-		@tweets = Tweet.all
+		@tweets = current_user.tweets
 	end
 
 	def create
@@ -10,9 +13,13 @@ class TweetsController < ApplicationController
 		@tweet.user = current_user
 		@tweet.save
 		#@tweet = Tweet.create(tweet_params)
-		@tweets = Tweet.all
+		@tweets = current_user.tweets
 		flash[:success] = "You have a created a tweet"
 		render 'new'
+	end
+
+	def index
+		@tweets = Tweet.all.reject {|tweet| tweet.user == current_user}
 	end
 
 	private
